d4e43bcfb6f3a0e0c847b52c061ef6337a00c263
Al June Damiles <aljune.36@gmail.com>
2025-05-11 22:15:32 +0800

    Update main.tsx remove obvious comment

--- TodoSample/src/main.tsx
+++ TodoSample/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { TodoProvider } from "./context/TodoContext"; // Import TodoProvider
+import { TodoProvider } from "./context/TodoContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
